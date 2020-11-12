cc09eb9c60330ca7d0c6a231cdc3e361e2228aae
cristiansk8 <cdpsk8@gmail.com>
2020-11-12 14:33:48 -0500

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer>
+  <div class="row">
+    <?php dynamic_sidebar('footer-widget-area'); ?>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ templates/_servicios-tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+  Template Name: Servicios Template
+*/
+
+	get_template_part('includes/header');
+
+
+	get_template_part('includes/footer');
